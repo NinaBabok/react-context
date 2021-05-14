e50d9c2e2d75813ed4e3f592d8ff29e8f472b3c8
Nikolina Babok <nbabok@foi.hr>
2021-05-14 19:48:26 +0200

    hands-on, lecture 11

---
+++ src/components/Article.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+
+const Article = () => {
+    const { isLoggedIn } = useContext(AuthContext);
+
+    return (
+        <>
+            <h2>Some article title</h2>
+            <p>Description content</p>
+            {isLoggedIn ? <button>Edit</button> : null}
+        </>
+    );
+}
+
+export default Article;
